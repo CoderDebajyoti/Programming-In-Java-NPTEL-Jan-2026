36183903d0b7b34ccf59e5b68767b2244bbf98da
CoderDebajyoti <debajyoti.475@gmail.com>
2026-02-14 10:40:27 +0530

    week3 done

---
+++ WEEK3/W03_P3.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class W03_P3{
+    public static void main(String[] args) {
+	   Scanner sc = new Scanner(System.in);
+	   int num=sc.nextInt();
+//Use while loop to count number of digits in an integer
+
+// Uncomment or create new
+int count = 0;
+  
+      while (num!=0) {
+        ++count;
+        num = num /10;
+      }
+  
+      System.out.print(count);
+}
+}
