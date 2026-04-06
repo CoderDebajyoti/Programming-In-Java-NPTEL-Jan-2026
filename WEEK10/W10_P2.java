6ca6f728256a5200203583b243427e1ac34fab98
CoderDebajyoti <debajyoti.475@gmail.com>
2026-04-06 09:25:57 +0530

    Create W10_P2.java

---
+++ WEEK10/W10_P2.java
@@ -0,0 +1,11 @@
+
+public class W10_P2 {
+    public static void main(String[] args) {
+        try {
+Class.forName("org.sqlite.JDBC");
+System.out.println(true);
+        } catch (Exception e) {
+            System.out.println(false);
+        }
+    }
+}
