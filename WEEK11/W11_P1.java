f3e9cd11ba38549a4cb3f7256f3ff476ef7275be
CoderDebajyoti <debajyoti.475@gmail.com>
2026-04-07 22:52:46 +0530

    Update W11_P1.java

--- WEEK11/W11_P1.java
+++ WEEK11/W11_P1.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 //Import required packages
 // Hint: USE static import by using *
 import java.sql.*;
+
 public class W11_P1 {
     public static void main(String args[]) {
         try {
@@ -9,7 +10,7 @@ public class W11_P1 {
             Statement stmt = null;
             String DB_URL = "jdbc:sqlite:/tempfs/db";
             System.setProperty("org.sqlite.tmpdir", "/tempfs");
-            } catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
